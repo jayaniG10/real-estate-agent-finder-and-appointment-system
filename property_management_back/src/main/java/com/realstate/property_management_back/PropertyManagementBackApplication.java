5b0d2fbe75e211448fdf0a89c15e296fc86fbe7e
PiyumiImashi <PiyumiImashi@users.noreply.github.com>
2026-04-18 10:15:00 +0530

    Initial backend setup and configuration for ratings

---
+++ property_management_back/src/main/java/com/realstate/property_management_back/PropertyManagementBackApplication.java
@@ -0,0 +1,13 @@
+package com.realstate.property_management_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementBackApplication.class, args);
+	}
+
+}
