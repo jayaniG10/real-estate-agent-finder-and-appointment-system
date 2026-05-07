e515bd3772a592d44cf90fb63dae8d333062e13d
Tennakoon I.M.S.R <it22363848@my.sliit.lk>
2026-05-07 16:00:00 +0530

    Refactor import declarations by stripping unused list handling packages

--- Backend/src/main/java/com/realstate/property_management_back/appointments/AppointmentsController.java
+++ Backend/src/main/java/com/realstate/property_management_back/appointments/AppointmentsController.java
@@ -1,6 +1,5 @@
 package com.realstate.property_management_back.appointments;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,18 +11,6 @@ public class AppointmentsController {
     @Autowired
     private AppointmentsService appointmentsService;
 
-    // READ (GET all)
-    @GetMapping
-    public List<AppointmentsDto> getAllAppointments() {
-        return appointmentsService.getAllAppointments();
-    }
-
-    // READ (GET by ID)
-    @GetMapping("/{id}")
-    public ResponseEntity<AppointmentsDto> getAppointmentById(@PathVariable Long id) {
-        return ResponseEntity.ok(appointmentsService.getAppointmentById(id));
-    }
-
     // CREATE (POST)
     @PostMapping
     public AppointmentsDto createAppointment(@RequestBody AppointmentsDto appointmentsDto) {
@@ -34,12 +21,5 @@ public class AppointmentsController {
     @PutMapping("/{id}")
     public ResponseEntity<AppointmentsDto> updateAppointment(@PathVariable Long id, @RequestBody AppointmentsDto appointmentsDto) {
         return ResponseEntity.ok(appointmentsService.updateAppointment(id, appointmentsDto));
-    }
-
-    // DELETE (DELETE)
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
-        appointmentsService.deleteAppointment(id);
-        return ResponseEntity.noContent().build();
     }
 }
